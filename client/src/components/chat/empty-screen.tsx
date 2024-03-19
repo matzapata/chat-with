a885b703cfcc4648fcccb314f0b529d7a8aec8c7
Matias Zapata <117381223+matzapatasphereone@users.noreply.github.com>
2024-03-19 11:02:10 +0000

    fixes

--- client/src/components/chat/empty-screen.tsx
+++ client/src/components/chat/empty-screen.tsx
@@ -1,39 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { IconArrowRight } from "@/components/ui/icons";
 
-import { Button } from '@/components/ui/button'
-import { IconArrowRight } from '@/components/ui/icons'
+const exampleMessages: { heading: string; message: string }[] = [];
 
-const exampleMessages = [
-  {
-    heading: 'Explain technical concepts',
-    message: `What is a "serverless function"?`
-  },
-  {
-    heading: 'Summarize an article',
-    message: 'Summarize the following article for a 2nd grader: \n'
-  },
-  {
-    heading: 'Draft an email',
-    message: `Draft an email to my boss about the following: \n`
-  }
-]
-// TODO: Update message
-
-export function EmptyScreen({ setInput }: any) { // TODO:  Pick<UseChatHelpers, 'setInput'>
+export function EmptyScreen({
+  setInput,
+}: {
+  setInput: (value: string) => void;
+}) {
   return (
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-white p-8">
-        <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Chatwith!
-        </h1>
+        <h1 className="mb-2 text-lg font-semibold">Welcome to Chatwith!</h1>
         <p className="mb-2 leading-normal text-gray-900">
-          Make questions about your file and get exactly what you need{' '}
-          .
+          Make questions about your file and get exactly what you need.
         </p>
 
-        {/* TODO: I think you need to remove examples */}
-        <p className="leading-normal text-gray-900">
-          You can start a conversation here or try the following examples:
-        </p>
+        {exampleMessages.length ? (
+          <p className="leading-normal text-gray-900">
+            You can start a conversation here or try the following examples:
+          </p>
+        ) : null}
         <div className="mt-4 flex flex-col items-start space-y-2">
           {exampleMessages.map((message, index) => (
             <Button
@@ -49,5 +36,5 @@ export function EmptyScreen({ setInput }: any) { // TODO:  Pick<UseChatHelpers, 
         </div>
       </div>
     </div>
-  )
+  );
 }
